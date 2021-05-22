63d73c5fdfc057cba3812a0afdaf124989d59cba
Hritik Arora <hritikarora875@gmail.com>
2021-05-22 13:16:28 +0530

    blog path added

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(index):
+    return HttpResponse('we are in home')
+def blog(index):
+    return HttpResponse('we are in blog')
