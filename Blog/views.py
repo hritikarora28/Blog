1cb87a68913977409ebd750ab5897fd9f7d36a34
Hritik Arora <hritikarora875@gmail.com>
2021-05-25 12:01:24 +0530

    html_added

--- Blog/views.py
+++ Blog/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def blogHome(request):
-    return HttpResponse('this is bloghome we will keep all the blogpost here')
+    return render(request,'blog/blogHome.html')
+    # return HttpResponse('this is bloghome we will keep all the blogpost here')
 
 
 def blogPost(request,slug):
-    return HttpResponse(f"this is blogPost {slug}")
+     return render(request,'blog/blogPost.html')
+    # return HttpResponse(f"this is blogPost {slug}")
--- home/views.py
+++ home/views.py
@@ -2,9 +2,12 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(index):
-    return HttpResponse('we are in request')
-def contact(index):
-    return HttpResponse('we are in request')
-def about(index):
-    return HttpResponse('we are in request')
+def home(request):
+    return render(request,'home/home.html')
+    # return HttpResponse('we are in home')
+def contact(request):
+    return render(request,'home/contact.html')
+    # return HttpResponse('we are in contact')
+def about(request):
+     return render(request,'home/about.html')
+    # return HttpResponse('we are in about')
