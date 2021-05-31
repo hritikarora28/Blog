059c3e99f633d60c08d0d1254cf3565a0ad392b0
Hritik Arora <hritikarora875@gmail.com>
2021-05-31 13:11:49 +0530

    contact added

--- home/views.py
+++ home/views.py
@@ -1,13 +1,22 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
     # return HttpResponse('we are in home')
 def contact(request):
+    if request.method =='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        msg = request.POST['msg']
+        print(f'{name},{email},{phone},{msg}')
+        contact = Contact(name=name, email=email,phone=phone, content=msg)
+        contact.save()
     return render(request,'home/contact.html')
-    # return HttpResponse('we are in contact')
+    
 def about(request):
      return render(request,'home/about.html')
     # return HttpResponse('we are in about')
