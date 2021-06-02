45eab4d88eb7175ac9e4143653ed831d05047733
Hritik Arora <hritikarora875@gmail.com>
2021-06-02 13:08:51 +0530

    message_added

--- home/views.py
+++ home/views.py
@@ -1,20 +1,27 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
     # return HttpResponse('we are in home')
 def contact(request):
+    
+    
     if request.method =='POST':
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
         msg = request.POST['msg']
         print(f'{name},{email},{phone},{msg}')
-        contact = Contact(name=name, email=email,phone=phone, content=msg)
-        contact.save()
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(msg)<4:
+            messages.error(request,"Please fill the form correctly")
+        else:
+            messages.success(request,"your call was sucessful")
+            contact = Contact(name=name, email=email,phone=phone, content=msg)
+            contact.save()
     return render(request,'home/contact.html')
     
 def about(request):
