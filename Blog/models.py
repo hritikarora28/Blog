215fbef0482c8ef03cefdeba4d6e4c53a0921d84
Hritik Arora <hritikarora875@gmail.com>
2021-07-02 13:29:18 +0530

    post_view_updated

--- Blog/models.py
+++ Blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=20)
     slug = models.CharField(max_length=130)
+    views= models.IntegerField(default=0)
     timestamp = models.DateTimeField(blank=True)
     def __str__(self):
         return self.title + ' by '+ self.author
